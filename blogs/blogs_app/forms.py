b77907cd88af53644261665aff13652a862eeb31
Ela <raziazaman@iut-dhaka.edu>
2025-02-03 19:14:32 +0600

    Update

--- blogs/blogs_app/forms.py
+++ blogs/blogs_app/forms.py
@@ -1,8 +1,10 @@
 from django import forms
-from .models import Blog
+from .models import Blog, Post
 
 class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
         fields = ['title']
         labels = {'title': ''}
+
+class 
